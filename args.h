24252204b270c24943a7284d341334651d26a5d8
Connor Olding <cloningdonor@gmail.com>
2012-08-04 05:23:29 -0700

    relicense to MIT

--- args.h
+++ args.h
@@ -1,19 +1,9 @@
-/*	args.h - argument handling
-	Copyright (C) 2012 Connor Olding
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright (C) 2012 Connor Olding
+ *
+ * This program is licensed under the terms of the MIT License, and
+ * is distributed without any warranty.  You should have received a
+ * copy of the license along with this program; see the file LICENSE.
+ */
 
 #ifndef ARGS_H_
 #define ARGS_H_
--- crc32.h
+++ crc32.h
@@ -1,19 +1,9 @@
-/*	crc32.h - 32-bit cyclic redundancy check calcuations
-	Copyright (C) 2012 Connor Olding
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright (C) 2012 Connor Olding
+ *
+ * This program is licensed under the terms of the MIT License, and
+ * is distributed without any warranty.  You should have received a
+ * copy of the license along with this program; see the file LICENSE.
+ */
 
 #ifndef CRC32_H_
 #define CRC32_H_
